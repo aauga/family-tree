0cfc11cde2ed833485222dc6957c75876047a20a
Adomas Auga <hello@auga.dev>
2021-04-10 23:19:14 +0300

    Added birth year TextField validation, improved validation of fields

--- src/main/controllers/AddPersonController.java
+++ src/main/controllers/AddPersonController.java
@@ -4,10 +4,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
 
+import java.sql.Date;
+import java.util.Calendar;
+
 public class AddPersonController {
     // Constants
     private static final int FULL_NAME_MAX_LENGTH = 48;
     private static final int PERSONAL_CODE_MAX_LENGTH = 11;
+    private static final int BIRTH_YEAR_MAX_LENGTH = 4;
     private static final int BIRTH_PLACE_MAX_LENGTH = 32;
 
     @FXML
@@ -15,6 +19,9 @@ public class AddPersonController {
 
     @FXML
     private TextField personalCodeTextField;
+
+    @FXML
+    private TextField birthYearTextField;
 
     public void initialize() {
 
@@ -47,7 +54,34 @@ public class AddPersonController {
     }
 
     @FXML
-    private void checkFields() {
-        //checkFullNameField();
+    private void formatBirthYearTextField() {
+        birthYearTextField.setTextFormatter(new TextFormatter<>(change -> {
+            String newText = change.getControlNewText();
+
+            if(newText.isEmpty() || (newText.length() <= BIRTH_YEAR_MAX_LENGTH && newText.matches("[0-9]+"))) {
+                return change;
+            }
+
+            return null;
+        }));
+    }
+
+    @FXML
+    private void checkIfBirthYearValid() {
+        String birthYearStr = birthYearTextField.getText();
+
+        if(!birthYearStr.isEmpty()) {
+            int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+            int birthYear = Integer.parseInt(birthYearStr);
+
+            if (birthYear > currentYear) {
+                birthYearTextField.setText(String.valueOf(currentYear));
+            }
+        }
+    }
+
+    @FXML
+    private void checkTextFields() {
+        checkIfBirthYearValid();
     }
 }
