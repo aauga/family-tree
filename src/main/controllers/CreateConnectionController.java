8988a55d6461758ba981d8ae973ef98ee6d0d808
Adomas Auga <hello@auga.dev>
2021-05-02 22:41:02 +0300

    Added choicebox selections

--- src/main/controllers/CreateConnectionController.java
+++ src/main/controllers/CreateConnectionController.java
@@ -1,4 +1,19 @@
 package main.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class CreateConnectionController {
+
+    @FXML
+    private ChoiceBox connectionType;
+
+    public void initialize() {
+        connectionType.getItems().add("Father");
+        connectionType.getItems().add("Mother");
+        connectionType.getItems().add("Child");
+        connectionType.getItems().add("Grandparent");
+        connectionType.getItems().add("Great-Grandparent");
+        connectionType.getItems().add("Other");
+    }
 }
